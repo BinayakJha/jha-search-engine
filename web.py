190d8e1efd4ab49d93a932bcc714e75a549f7d74
Binayak Jha <binayakjha@Binayak-MacBook-Air.local>
2021-08-30 14:58:14 +0545

    added lyrics search function

--- web.py
+++ web.py
@@ -1310,58 +1310,61 @@ if query:
                     yt_url1 = f"https://i.ytimg.com/vi/{featured_answer1}/0.jpg"
                     st.markdown(f'<a href="{yt_url1}" target="_blank"><iframe height="400" src="https://www.youtube.com/embed/{featured_answer1}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style = "width:90%;border-radius:5px;"></iframe></a>', unsafe_allow_html=True)
                     st.header(f"[{title}]({featured_answer})")
-                elif 'lyrics' in query:
-                             # replace spaces with +
-                            lyrics_url = "https://www.google.com/search?q=" + query
-                            print(lyrics_url)
-                            session = HTMLSession()
-                            response = session.get(lyrics_url)
-                            try:
-                                para1 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[1]')
-                                st.markdown(para1[0].text)
-                            except IndexError:
-                                pass
-
-                            try:
-                                para2 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[2]')
-                                st.markdown(para2[0].text)
-                            except IndexError:
-                                pass
-
-                            try:
-                                para3 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[3]')
-                                st.markdown(para3[0].text)
-                            except IndexError:
-                                pass
-
-                            try:
-                                para4 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[4]')
-                                st.markdown(para4[0].text)
-                            except IndexError:
-                                pass
-
-                            try:
-                                para5 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[5]')
-                                st.markdown(para5[0].text)
-                            except IndexError:
-                                pass
-                            try:
-                                para6 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[6]')
-                                st.markdown(para6[0].text)
-                            except IndexError:
-                                pass
-                            try:
-                                para7 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[7]')
-                                st.markdown(para7[0].text)
-                            except IndexError:
-                                pass
-                            try:
-                                para8 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[8]')
-                                st.markdown(para8[0].text)
-                            except IndexError:
-                                pass
+               
                 else:
                     st.markdown(featured_answer, unsafe_allow_html=True)
+                try:
+                             # replace spaces with +
+                        lyrics_url = "https://www.google.com/search?q=" + query
+                        print(lyrics_url)
+                        session = HTMLSession()
+                        response = session.get(lyrics_url)
+                        try:
+                            para1 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[1]')
+                            st.markdown(para1[0].text)
+                        except IndexError:
+                            pass
+
+                        try:
+                            para2 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[2]')
+                            st.markdown(para2[0].text)
+                        except IndexError:
+                            pass
+
+                        try:
+                            para3 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[3]')
+                            st.markdown(para3[0].text)
+                        except IndexError:
+                            pass
+
+                        try:
+                            para4 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[4]')
+                            st.markdown(para4[0].text)
+                        except IndexError:
+                            pass
+
+                        try:
+                            para5 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[5]')
+                            st.markdown(para5[0].text)
+                        except IndexError:
+                            pass
+                        try:
+                            para6 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[6]')
+                            st.markdown(para6[0].text)
+                        except IndexError:
+                            pass
+                        try:
+                            para7 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[7]')
+                            st.markdown(para7[0].text)
+                        except IndexError:
+                            pass
+                        try:
+                            para8 =  response.html.xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div[8]')
+                            st.markdown(para8[0].text)
+                        except IndexError:
+                            pass
+                except IndexError:
+                    pass
                 
                 if featured_answer == "":
                     try:
